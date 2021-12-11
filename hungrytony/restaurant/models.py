bf0768c52feb6963ca1cd17cf36168a391b75102
Aleks Nikiforov <aleks59niki30@gmail.com>
2021-12-11 14:50:08 +0300

    create base templates

--- hungrytony/restaurant/models.py
+++ hungrytony/restaurant/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Table(models.Model):
+    number = models.CharField(max_length=32, verbose_name='номер столика')
     capacity = models.PositiveIntegerField(verbose_name='вместимость')
     is_free = models.BooleanField(default=True, verbose_name='свободен')
     url = models.CharField(max_length=64, verbose_name='ссылка')
 
     def __str__(self):
-        return f'{self.pk}-{self.url}'
+        return f'Столик №{self.number}'
 
     class Meta:
         verbose_name = 'столик'
@@ -18,7 +19,7 @@ class TableInfo(models.Model):
     table_id = models.OneToOneField(Table, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=32, verbose_name='имя столика')
     info = models.TextField(verbose_name='описание столика')
-    photo = models.ImageField(upload_to='restaurant/tables/')
+    photo = models.ImageField(upload_to='images/tables/')
 
     def __str__(self):
         return f'{self.name}'
